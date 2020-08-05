e727edd0785b31ca4168481dab61228a4a474aba
fengchezhi <619146134@qq.com>
2020-08-06 00:20:26 +0800

    更新configProvider

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -18,21 +18,48 @@ class ConfigProvider
 {
     public function __invoke(): array
     {
-        // /** @var ContainerInterface $container */
-        // $container = ApplicationContext::getContainer();
-        // $router = $container->get(Router::class);
-        // $router->get('/demo', '\App\Controller\IndexController@index');
-
         return [
             'dependencies' => [
+                \Hyperf\HttpServer\CoreMiddleware::class => \Wind\Telescope\Middleware\CoreMiddleware::class,
             ],
             'commands' => [
+            ],
+            'view' => [
+                'engine' => \Wind\Telescope\TemplateEngine::class,
+                'mode' => \Hyperf\View\Mode::SYNC,
+                'config' => [
+                    'view_path' => BASE_PATH . '/vendor/windawake/hyperf-telescope/storage/view/',
+                    'cache_path' => BASE_PATH . '/runtime/view/',
+                ],
+            ],
+            'server' => [
+                'settings' => [
+                    // 静态资源
+                    'document_root' => BASE_PATH . '/vendor/windawake/hyperf-telescope/public',
+                    'enable_static_handler' => true,
+                ],
             ],
             'annotations' => [
                 'scan' => [
                     'paths' => [
                         __DIR__,
                     ],
+                ],
+            ],
+            'exceptions' => [
+                'handler' => [
+                    'http' => [
+                        \Qbhy\HyperfAuth\AuthExceptionHandler::class,
+                    ]
+                ]
+            ],
+            'publish' => [
+                [
+                    'id' => 'auth',
+                    'description' => 'auth 组件配置.', // 描述
+                    // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
+                    'source' => __DIR__ . '/../publish/file.php',  // 对应的配置文件路径
+                    'destination' => BASE_PATH . '/config/autoload/file.php', // 复制为这个路径下的该文件
                 ],
             ],
         ];
