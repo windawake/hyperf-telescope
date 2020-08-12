12063a997c9cb9686ecf850b40261b9edd0db3ea
zhangzibin <zhangzibin@ecomxhat.com>
2020-08-12 18:34:06 +0800

    压缩js

--- storage/view/index.blade.php
+++ storage/view/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="robots" content="noindex, nofollow">
-
+    <link rel="shortcut icon" href="/vendor/telescope/favicon.ico">
     <title>Hyperf Telescope</title>
 
     <!-- Style sheets-->
